aaea15f499c72c3f8a18c9c28b1b1f540f164e5f
dcoffill <decoffill@gmail.com>
2014-03-22 21:44:59 -0700

    Minor LoadAvg refactoring, added javadoc dir to gitignore

--- src/com/nguyenmp/csil/LoadAvg.java
+++ src/com/nguyenmp/csil/LoadAvg.java
@@ -11,16 +11,38 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by david on 3/20/14.
+ * A {@link com.nguyenmp.csil.concurrency.CommandExecutor} that gets system
+ * load average and prints to the console in a tabular format.  This will
+ * help users decide which machine to use
  */
-public class LoadAvg {
+public class LoadAvg extends CommandExecutor {
+	public static final String COMMAND = "cat /proc/loadavg";
+	public final String hostname;
+
+	public LoadAvg(String hostname) {
+		super(hostname, COMMAND);
+		this.hostname = hostname;
+
+	}
+	@Override
+	public void onSuccess(String result) {
+		// Parse the line to get load averages only
+		String[] avgs = result.split("\\s+");
+		System.out.format("%-10s: %s  %s  %s\n", hostname.split("\\.")[0], avgs[0], avgs[1], avgs[2]);
+	}
+
+	@Override
+	public void onError(Exception e) {
+		System.err.println("Error: could not connect to " + hostname);
+	}
+
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		Database db = new Database();
 		List<Computer> computerList = db.computers.getActiveComputers();
 		int processors = Runtime.getRuntime().availableProcessors();
 		ExecutorService service = Executors.newFixedThreadPool(processors * 2);
 		for (Computer c: computerList) {
-			LoadAvgRunner runner = new LoadAvgRunner(c.hostname);
+			LoadAvg runner = new LoadAvg(c.hostname);
 			service.execute(runner);
 		}
 		service.shutdown();
@@ -28,35 +50,6 @@ public class LoadAvg {
 			service.awaitTermination(9999, TimeUnit.DAYS);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}
-	}
-
-
-	/**
-	 * A {@link com.nguyenmp.csil.concurrency.CommandExecutor} that gets system
-	 * load average and prints to the console in a tabular format.  This will
-	 * let users choose which
-	 */
-
-	private static class LoadAvgRunner extends CommandExecutor {
-		public static final String COMMAND = "cat /proc/loadavg";
-		public final String hostname;
-
-		public LoadAvgRunner(String hostname) {
-			super(hostname, COMMAND);
-			this.hostname = hostname;
-
-		}
-		@Override
-		public void onSuccess(String result) {
-			// Parse the line to get load averages only
-			String[] avgs = result.split("\\s+");
-			System.out.format("%-10s: %s  %s  %s\n", hostname.split("\\.")[0], avgs[0], avgs[1], avgs[2]);
-		}
-
-		@Override
-		public void onError(Exception e) {
-			System.err.println("Error: could not connect to " + hostname);
 		}
 	}
 }
